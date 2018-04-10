1c34246e3a8d06fd9157d20215fc3dffdc122acb
netwelfare <didawangxiaofei@126.com>
2018-04-10 18:04:42 +0800

    修改加群链接

--- wordpress/wp-content/themes/swiftlet/single.php
+++ wordpress/wp-content/themes/swiftlet/single.php
@@ -115,8 +115,9 @@ if(have_posts()): while (have_posts()&&$pos<=6) : the_post(); update_post_caches
             	
 				<?php   $custom_fields = get_post_custom_keys($post_id);
 				if (!in_array ('copyright', $custom_fields)) : ?>
+				
 				<div class="postcopyright">
-					<p><strong> 声明: </strong> 本文由<a href="<?php bloginfo('home'); ?>">金丝燕网</a>原创编译，转载请保留链接: <a href="<?php the_permalink()?>" title=<?php the_title(); ?>><?php the_title(); ?></a>，更多交流请加群：278721352
+					<p><strong> 声明: </strong> 本文由<a href="<?php bloginfo('home'); ?>">金丝燕网</a>原创编译，转载请保留链接: <a href="<?php the_permalink()?>" title=<?php the_title(); ?>><?php the_title(); ?></a>，更多交流请加群：293074111
 					 </p>
 				</div>
 				
